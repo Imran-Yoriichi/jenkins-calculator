b3e1d949e327c9e6054bb0a3cb789391657501f9
Imran-Yoriichi <khajaimran.s10@gmail.com>
2026-04-05 11:48:47 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    if add(2, 3) == 5
+    return add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
